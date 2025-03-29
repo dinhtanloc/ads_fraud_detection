fb4debca325debbe64a20aa8b27bfe4e9026a34d
dinhtanloc <locdinh.31221020226@st.ueh.edu.vn>
2025-03-29 16:24:54 +0700

    update import module

--- libs/common.py
+++ libs/common.py
@@ -1,282 +1,77 @@
-import os, sys
-import pandas as pd
-import numpy as np
-import random as rd
-from joblib import dump
-import matplotlib.pyplot as plt
-# %matplotlib inline
-import seaborn as sns
-sns.set_style('darkgrid')
-import warnings
-warnings.filterwarnings('ignore')
-import joblib
-from sklearn.ensemble import VotingClassifier
-from scipy.stats import boxcox, yeojohnson
-from sklearn.preprocessing import QuantileTransformer
-import gc
-from plotly import __version__
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-# import cufflinks as cf
-# cf.go_offline()
-from plotly.offline import iplot
-import plotly.graph_objs as go
-import plotly.express as px
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
 import datetime
-import missingno as msno
-from tensorflow.keras.layers import Input, Dense, Lambda
-from tensorflow.keras.models import Model
-from tensorflow.keras.losses import categorical_crossentropy
-from tensorflow.keras import backend as K
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Concatenate
-
-import os, sys
-import pandas as pd
-import numpy as np
-import random as rd
-import matplotlib.pyplot as plt
-# %matplotlib inline
-import seaborn as sns
-sns.set_style('darkgrid')
-import warnings
-warnings.filterwarnings('ignore')
-
-from plotly import __version__
-from plotly.subplots import make_subplots
-
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-import cufflinks as cf
-cf.go_offline()
-# from pandas_profiling import ProfileReport
-
-from sklearn.model_selection import train_test_split
-from sklearn.utils.class_weight import compute_class_weight
-from sklearn.preprocessing import LabelEncoder,MinMaxScaler,OneHotEncoder,StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.feature_selection import RFE
-
-from numpy import set_printoptions
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.svm import SVC
-from sklearn.feature_selection import SelectFromModel
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, KFold
-
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.naive_bayes import MultinomialNB
-from keras.models import Sequential
-from keras.layers import Dense, BatchNormalization, Dropout
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.base import clone
-from mlxtend.classifier import StackingClassifier
-from kerastuner.tuners import RandomSearch
-from keras_tuner import Objective
-
-from scipy.stats import uniform
-from scikeras.wrappers import KerasClassifier, KerasRegressor
-from imblearn.over_sampling import SMOTE
-
-
-import numpy as np
-
-
-
-
-
-import tensorflow as tf
-
-# from pydantic_settings import BaseSettings # NEW
-
-# from pandas_profiling import ProfileReport
-
-from sklearn.model_selection import train_test_split
-from sklearn.utils.class_weight import compute_class_weight
-from sklearn.preprocessing import LabelEncoder,MinMaxScaler,OneHotEncoder,StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout,Concatenate
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.feature_selection import RFE
-
-from tensorflow import keras
-from keras.activations import relu
-from tensorflow.keras.layers import *
-from tensorflow.keras import Model
-from tensorflow.keras import layers as Layers
-from numpy import set_printoptions
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.svm import SVC
-from sklearn.feature_selection import SelectFromModel
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, KFold
-
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.naive_bayes import MultinomialNB
-
-from keras.models import Sequential
-from keras.layers import Dense, BatchNormalization, Dropout
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.base import clone
-# from mlxtend.classifier import StackingClassifier
-# from sklearn.ensemble import StackingClassifier
-
-# from kerastuner.tuners import RandomSearch
-from scipy.stats import uniform
-import numpy as np
-
-from keras.callbacks import EarlyStopping, ModelCheckpoint, LearningRateScheduler
-from keras.layers import Conv1D, MaxPooling1D, GlobalAveragePooling1D, GlobalMaxPooling1D, Dense, Flatten
-from keras.models import Model
-from keras.layers import Input
-import tensorflow as tf
-from sklearn.model_selection import StratifiedKFold
-
-
-from tabnet_keras import TabNetRegressor, TabNetClassifier
-from tensorflow.keras.optimizers import Adam
-from keras.regularizers import l2
-
-from keras.models import Sequential
-from keras.layers import Dense, BatchNormalization, Dropout
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.base import clone
-# from mlxtend.classifier import StackingClassifier
-# from kerastuner.tuners import RandomSearch
-from scipy.stats import uniform
-import numpy as np
-import wandb
 import enum
+import itertools
 import math
+import os
+import pickle
+import random as rd
+import re
+import sys
 import time
-from copy import deepcopy
 import warnings
-import typing as ty
+from copy import deepcopy
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union, cast
-import shap
-import lime
 
+import matplotlib.pyplot as plt
+import missingno as msno
 import numpy as np
 import pandas as pd
-from numpy import mean
-from numpy import std
-
-import matplotlib.pyplot as plt
-# %matplotlib inline
-import seaborn as sns
-plt.style.use('ggplot')
-from plotly import tools
-import plotly.offline as py
-import plotly.figure_factory as ff
-py.init_notebook_mode(connected=True)
-import plotly.graph_objs as go
 import plotly.express as px
-from plotly.subplots import make_subplots
-
-
-
-from scipy.stats import norm
-from sklearn.preprocessing import StandardScaler
-from scipy import stats
-
-import warnings
-warnings.filterwarnings('ignore')
-
-#for displaying 500 results in pandas dataframe
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-
-import itertools
-import xgboost as xgb
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, roc_curve, precision_recall_curve
-import warnings
-
-
-import  numpy as np
-import  pandas as pd
+import plotly.graph_objs as go
 import seaborn as sns
-import matplotlib.pyplot as plt
-import re
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-# from keras.preprocessing.text import Tokenizer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split,GridSearchCV,RandomizedSearchCV
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score,recall_score, f1_score, confusion_matrix,precision_score
-from scipy.sparse import hstack,csr_matrix
-from wordcloud import WordCloud
-from spacy.lang.en.stop_words import STOP_WORDS
+import shap
 import spacy
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import LinearSVC
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import FunctionTransformer
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, LSTM, Dropout,Input
-from sklearn import preprocessing
-from tensorflow.keras.models import Model
-import scipy.sparse as sp
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
-from sklearn.ensemble import GradientBoostingClassifier
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import xgboost as xgb
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from sklearn.preprocessing import LabelEncoder
-import pickle
-from sklearn.model_selection import learning_curve,cross_val_score
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras import backend as K, layers, models
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.layers import *
+from keras.models import Model, Sequential
+from keras.preprocessing.sequence import pad_sequences
+from keras.regularizers import l2
 from lightgbm import LGBMClassifier
-from xgboost import XGBClassifier
-from scipy import sparse
-import warnings
-import numpy as np
-
-from keras.layers import Input, Conv1D, BatchNormalization, Activation, Dropout, Reshape, GlobalAveragePooling1D,MaxPooling1D,Dense,AveragePooling1D
-from keras.models import Model
-from keras import layers
-
-from keras.layers import Layer
-from keras import backend as K
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from plotly.offline import iplot, plot
+from plotly.subplots import make_subplots
+from scipy import sparse, stats
+from scipy.sparse import csr_matrix, hstack
+from scipy.stats import boxcox, norm, uniform, yeojohnson
+from sklearn import preprocessing
+from sklearn.compose import ColumnTransformer
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import (
+    GradientBoostingClassifier, RandomForestClassifier, VotingClassifier
+)
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_selection import (
+    RFE, SelectFromModel, SelectKBest, f_classif
+)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score, auc, classification_report, confusion_matrix,
+    precision_recall_curve, precision_score, recall_score, roc_curve
+)
+from sklearn.model_selection import (
+    GridSearchCV, KFold, RandomizedSearchCV, StratifiedKFold,
+    train_test_split, cross_val_score, learning_curve
+)
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.preprocessing import (
+    FunctionTransformer, LabelEncoder, MinMaxScaler, OneHotEncoder,
+    QuantileTransformer, StandardScaler
+)
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+from spacy.lang.en.stop_words import STOP_WORDS
+from tabnet_keras import TabNetClassifier, TabNetRegressor
+from tensorflow.keras import layers as Layers
+from tensorflow.keras.layers import Input
+from tensorflow.keras.optimizers import Adam
+from wordcloud import WordCloud
+import lime
+import wandb
